cb40c7d7e3248be3b4f8f5c5719e5c721aa43801
DANIEL KOKONYA NYONGESA <dkokonya@outlook.com>
2023-05-09 10:07:47 +0300

    added a second activity

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -14,6 +16,18 @@ class MainActivity : AppCompatActivity() {
         Log.i("MainActivity","button was clicked")
         btnShowToast.setOnClickListener() {
             Toast.makeText(this, "daniel kokonya", Toast.LENGTH_LONG).show()
+        }
+
+        val userMessage: TextView = findViewById(R.id.usermessage)
+        userMessage.setOnClickListener {
+            Toast.makeText(this, "second button wsa clicked",Toast.LENGTH_LONG)
+
+            val sendtonnextact : Button = findViewById(R.id.sendtonnextact)
+            val message: String = sendtonnextact.text.toString()
+            Toast.makeText(this,message,Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, Second_activity :: class.java)
+            startActivity(intent)
         }
     }
 }
